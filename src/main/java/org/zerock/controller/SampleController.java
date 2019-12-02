fa516765ae1d851fb2c42cca9aa9602543e6b535
jisun.park <lovelyjs9075@gmail.com>
2019-12-03 00:16:14 +0900

    test2

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,13 +1,12 @@
 package org.zerock.controller;
-import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Log4j
 public class SampleController {
-    @GetMapping("/doA")
-    public void doA() {
-        log.info("test");
+    @RequestMapping(value = "/sample")
+    public String sample() {
+        return "welcome";
     }
 }
---
+++ web/WEB-INF/views/sample.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lovel
+  Date: 2019-12-02
+  Time: 오후 11:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+dfgdg
+</body>
+</html>
